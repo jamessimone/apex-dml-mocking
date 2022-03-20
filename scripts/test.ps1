9e289345c7892a43f7f86fa50b86b96a1b13cd99
james simone <jamirus99@gmail.com>
2022-03-19 20:23:48 -0600

    Added project-scratch-def config file, properly writing Apex test output to check the outcome

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,7 +2,7 @@ $DebugPreference = 'Continue'
 $ErrorActionPreference = 'Stop'
 # This is also the same script that runs on Github via the Github Action configured in .github/workflows - there, the
 # DEVHUB_SFDX_URL.txt file is populated in a build step
-$testInvocation = 'npx sfdx force:apex:test:run -s ApexDmlMockingSuite -r human -w 20'
+$testInvocation = 'npx sfdx force:apex:test:run -s ApexDmlMockingSuite -r human -w 20 -d ./tests/apex'
 $scratchOrgName = 'apex-dml-mocking-scratch'
 
 function Start-Tests() {
